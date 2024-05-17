012c676415660f0124f7a5fb34dbd07bbef1c205
Yurii Miroshnyk <lemadioro@gmail.com>
2024-05-17 23:56:12 +0300

    Add main (Home) page. Create IndexController and its view.

---
+++ app/Http/Controllers/Main/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Main;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Foundation\Application as ContractsApplication;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application;
+
+class IndexController extends Controller
+{
+    public function __invoke(): View | Factory | Application | ContractsApplication
+    {
+        return view('main.index');
+    }
+}
---
+++ resources/views/main/index.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.main')
+
+@section('content')
+<main class="main">
+    <div class="container">
+        <div class="page__text">
+            <div>
+                <img src="{{ asset('assets/images/LOGO.png') }}" alt="logo">
+            </div>
+            <div class="col-md-5">
+            <p>
+                Random number generation service.
+            </p>
+            <p>
+                <h4>Specifications:</h4>
+                <ol>
+                    <li>
+                        REST API for random number generation.
+                    </li>
+                    <li>
+                        The unique <code>id</code> by which you can get the result generation.
+                    </li>
+                    <li>
+                        Two public API methods: <code>generate()</code> and <code>retrieve(id)</code>.
+                    </li>
+                </ol>
+            </p>
+
+            <p>
+                <a href="#" class="btn btn-success">Api documentation</a>
+            </p>
+            </div>
+        </div>
+    </div>
+
+</main>
+
+@endsection
